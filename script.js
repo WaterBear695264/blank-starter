c3356526d1490f95124909a1fb80a997d963e2a7
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2025-08-15 18:39:52 +0000

    added a way to save your email with a text box

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.getElementById("button").addEventListener("click",myFunc);
-
-function myFunc(){
-    document.getElementById("output").innerHTML = "hi friend!";
+document.getElementById("button").addEventListener("click",save);
+document.getElementById("button2").addEventListener("click",retrieve);
+let output = document.getElementById("output")
+let info = {email: ""};
+function save(){
+    info.email = document.getElementById("scaminator5000").value;
+    let stringified = JSON.stringify(info);
+    localStorage.setItem("info", stringified);
+    console.log(localStorage)
 }
 
+function retrieve () {
+    let retrieveal = localStorage.getItem("info")
+    let retrieved = JSON.parse(retrieveal)
+    console.log(retrieved)
+    output.innerHTML = retrieved.email
+}
