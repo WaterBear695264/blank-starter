00862e729e19d4d2e472a825eb5f5160c8ad5ec7
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2025-08-19 18:41:39 +0000

    worked on making a canvas and making the table, debugging the table a bit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.getElementById("button").addEventListener("click",save);
 document.getElementById("button2").addEventListener("click",retrieve);
-let output = document.getElementById("output")
+document.getElementById("make").addEventListener("click",makeTable);
+const output = document.getElementById("output")
 let info = {email: ""};
-
-
 
 function save(){
     info.email = document.getElementById("scaminator5000").value;
@@ -17,4 +16,38 @@ function retrieve () {
     let retrieved = JSON.parse(retrieveal)
     console.log(retrieved)
     output.innerHTML = retrieved.email
+}
+
+function makeTable(){
+    const table = document.querySelector("#cityTable tbody");
+
+    for(let i = 0; i < computerdata.length; i++){
+        let row = document.createElement("tr")
+        
+        let num = document.createElement("td")
+        num.innterHTML = i+1;
+
+        let modelname = document.createElement("td")
+        modelname.innterHTML = computerdata[i].model_name;
+
+        let ram = document.createElement("td")
+        ram.innterHTML = computerdata[i].ram;
+
+        let storage = document.createElement("td")
+        storage.innterHTML = computerdata[i].storage;
+
+        let CPU = computerdata[i].gpu;
+        CPU.innerHTML = computerdata.gpu
+
+        let GPU = computerdata[i].gpu;
+        GPU.innerHTML = computerdata.gpu
+
+        row.appendChild(num)
+        row.appendChild(modelname)
+        row.appendChild(ram)
+        row.appendChild(storage)
+        row.appendChild(CPU)
+        row.appendChild(GPU)
+        table.appendChild(row)
+    }
 }
